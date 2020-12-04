60ea38bb341b3341ab62a91c1aceedffd85a1f52
jddupuy <dupuy.juan@gmail.com>
2020-12-04 15:49:52 -0500

    Backup branch.

--- app/src/main/java/com/example/cookingapp/MainActivity.java
+++ app/src/main/java/com/example/cookingapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.cookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 import android.os.Bundle;
+import android.app.Activity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button ingredientButton = (Button) findViewById(R.id.ingredient_button);
+
+        ingredientButton.setOnClickListener(new View.onClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                MainActivity.setContentView(R.layout.activity_ingredients__screen);
+            }
+
+        });
+
     }
+
 }
