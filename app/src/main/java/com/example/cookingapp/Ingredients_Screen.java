0d04b04a4e2fab985436434596db5ba45bc94ce5
jddupuy <dupuy.juan@gmail.com>
2020-12-08 00:08:02 -0500

    Added Checkboxes to Ingredients Screen. Working on Button Control between Main Activity and Ingredients Activity.

--- app/src/main/java/com/example/cookingapp/Ingredients_Screen.java
+++ app/src/main/java/com/example/cookingapp/Ingredients_Screen.java
@@ -1,8 +1,15 @@
+//Ingredients Screen
+//Allows users to enter ingredients that they have
+//Juan Dupuy
+
 package com.example.cookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Ingredients_Screen extends AppCompatActivity {
 
@@ -10,5 +17,17 @@ public class Ingredients_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredients__screen);
+
+        configureBackButton();
+    }
+
+    private void configureBackButton(){
+        Button backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+           public void onClick(View view){
+               finish();
+           }
+        });
     }
 }
--- app/src/main/java/com/example/cookingapp/MainActivity.java
+++ app/src/main/java/com/example/cookingapp/MainActivity.java
@@ -1,8 +1,15 @@
+//Mainscreen Activity
+//Hosts two Buttons, Ingredients and Recipes
+//Juan Dupuy
+
 package com.example.cookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        configureIngredientButton();
     }
+
+    private void configureIngredientButton(){
+        Button nextButton = (Button) findViewById(R.id.ingredient_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(MainActivity.this, Ingredients_Screen.class));
+            }
+
+        });
+    }
+
 }
